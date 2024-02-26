42f6c4d589af63db37612d2ba81aaf85ec35cb49
Hhy <hsuihung@163.com>
2024-02-26 12:03:43 +0800

    增加SpringSecurity认证和授权

--- src/main/java/cn/hhy/communitysystem/controller/LoginController.java
+++ src/main/java/cn/hhy/communitysystem/controller/LoginController.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -65,7 +66,7 @@ public class LoginController implements CommunityConstant {
             model.addAttribute("usernameMsg", map.get("usernameMsg"));
             model.addAttribute("passwordMsg", map.get("passwordMsg"));
             model.addAttribute("emailMsg", map.get("emailMsg"));
-            return "register.back";
+            return "/site/register";
         }
     }
 
@@ -154,6 +155,7 @@ public class LoginController implements CommunityConstant {
     @RequestMapping(path = "/logout", method = RequestMethod.GET)
     public String logout(@CookieValue("ticket") String ticket) {
         userService.logout(ticket);
+        SecurityContextHolder.clearContext();
         return "redirect:/login";
     }
 
--- src/main/java/cn/hhy/communitysystem/util/RedisKeyUtil.java
+++ src/main/java/cn/hhy/communitysystem/util/RedisKeyUtil.java
@@ -10,6 +10,21 @@ public class RedisKeyUtil {
     private static final String PREFIX_TICKET = "ticket";
     private static final String PREFIX_USER = "user";
 
+    /**
+     * 网站访客
+     */
+    private static final String PREFIX_UV = "uv";
+
+    /**
+     * 网站日活用户
+     */
+    private static final String PREFIX_DAU = "dau";
+
+    /**
+     * 记录发生"变化"的帖子，定时更新排名分数
+     */
+    private static final String PREFIX_POST = "post";
+
     // 某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
     public static String getEntityLikeKey(int entityType, int entityId) {
@@ -48,4 +63,30 @@ public class RedisKeyUtil {
     public static String getUserKey(int userId) {
         return PREFIX_USER + SPLIT + userId;
     }
+
+    // 单日UV
+    public static String getUVKey(String date) {
+        return PREFIX_UV + SPLIT + date;
+    }
+
+    // 区间UV
+    public static String getUVKey(String startDate, String endDate) {
+        return PREFIX_UV + SPLIT + startDate + SPLIT + endDate;
+    }
+
+    // 单日活跃用户
+    public static String getDAUKey(String date) {
+        return PREFIX_DAU + SPLIT + date;
+    }
+
+    // 区间活跃用户
+    public static String getDAUKey(String startDate, String endDate) {
+        return PREFIX_DAU + SPLIT + startDate + SPLIT + endDate;
+    }
+
+    // 帖子分数
+    public static String getPostScoreKey() {
+        return PREFIX_POST + SPLIT + "score";
+    }
+
 }
